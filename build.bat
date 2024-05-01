32b07d1ac9fe1760c838223792185cd649829d9e
tanukimaru0090 <tanusan1015@gmail.com>
2024-05-02 03:17:38 +0900

    Update

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cl /EHsc main.cc /I"C:\Users\tanukimaru\use-bin\freeglut-3.4.0\include" /link /LIBPATH:"C:\Users\tanukimaru\use-bin\freeglut-3.4.0\build\lib\Debug" freeglutd.lib
+cl /O2 /DNDEBUG /EHsc  main.cc /I"C:\Users\tanukimaru\use-bin\freeglut-3.4.0\include" /link /LIBPATH:"C:\Users\tanukimaru\use-bin\freeglut-3.4.0\build\lib\Release" freeglut.lib
